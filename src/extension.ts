ce67811813b18f274990343d0dfbe5bd60533b4a
Alexandr Petyaykin <a.petyaykin@infomaximum.com>
2021-04-01 12:29:58 +0300

    feat: реализовал замену

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,125 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+/* eslint-disable @typescript-eslint/naming-convention */
+import {
+  window,
+  workspace,
+  commands,
+  Disposable,
+  ExtensionContext,
+  TextDocument,
+  Position,
+  Range,
+  TextDocumentWillSaveEvent,
+} from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { readdirSync, lstatSync } from "fs";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "importer" is now active!');
+const isDirectory = (source: string) => lstatSync(source).isDirectory();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('importer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+const getDirectories = (source: string) =>
+  readdirSync(source)
+    .map((name) => ({ source, name }))
+    .filter((e) => isDirectory(e.source));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from importer!');
-	});
+const isTypescript = (languageId: TextDocument["languageId"]) =>
+  languageId === "typescript" || languageId === "typescriptreact";
 
-	context.subscriptions.push(disposable);
+const isJavascript = (languageId: TextDocument["languageId"]) =>
+  languageId === "javascript" || languageId === "javascriptreact";
+
+const isSupportedLanguage = ({ languageId }: TextDocument) =>
+  isTypescript(languageId) || isJavascript(languageId);
+
+const importRegex = new RegExp(
+  /import(?:["'\s]*([\w*${}\n\r\t, ]+)from\s*)?["'\s]["'\s](.*[@\w_-]+)["'\s].*;$/,
+  "mg"
+);
+
+export function activate(ctx: ExtensionContext) {
+  console.log("Typescript MonoRepo Import Helper is now active");
+
+  const importFixer = new ImportFixer();
+  const controller = new ImportFixerController(importFixer);
+
+  ctx.subscriptions.push(controller);
+  ctx.subscriptions.push(importFixer);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export class ImportFixer {
+  public checkForBrokenImports(doc: TextDocument) {
+    const editor = window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    if (doc !== editor.document) {
+      return;
+    }
+
+    if (!isSupportedLanguage(doc)) {
+      return;
+    }
+
+    const packagesDirectoryMatch = doc.fileName.match(
+      /(.*\/packages)\/[^\/]*\//
+    );
+
+    if (!packagesDirectoryMatch) {
+      return;
+    }
+
+    const packagesDirectory = packagesDirectoryMatch[1];
+
+    const modules = getDirectories(packagesDirectory).map((e) => e.name);
+
+    const match = doc.getText().matchAll(importRegex);
+
+    editor.edit((builder) => {
+      while (true) {
+        const mathValue = match.next();
+
+        if (mathValue.done) {
+          break;
+        }
+
+        const importPath = mathValue.value[2];
+
+        const name = "@im/";
+
+        if (modules.some((m) => importPath.includes(m))) {
+          if (!importPath.includes(name) && mathValue.value.index) {
+            builder.insert(
+              doc.positionAt(doc.getText().indexOf(importPath)),
+              name
+            );
+          }
+        }
+      }
+    });
+  }
+
+  public dispose() {}
+}
+
+class ImportFixerController {
+  private _importFixer: ImportFixer;
+  private _disposable: Disposable;
+
+  constructor(ImportFixer: ImportFixer) {
+    this._importFixer = ImportFixer;
+
+    // subscribe to selection change and editor activation events
+    let subscriptions: Disposable[] = [];
+    workspace.onWillSaveTextDocument(this._onEvent, this, subscriptions);
+
+    // create a combined disposable from both event subscriptions
+    this._disposable = Disposable.from(...subscriptions);
+  }
+
+  private _onEvent(event: TextDocumentWillSaveEvent) {
+    this._importFixer.checkForBrokenImports(event.document);
+  }
+
+  public dispose() {
+    this._disposable.dispose();
+  }
+}
